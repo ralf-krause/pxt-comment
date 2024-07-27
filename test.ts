b5d907f02ce243ccdf84047a3e714da7acbbd7f5
Ralf Krause <27956839+ralf-krause@users.noreply.github.com>
2024-07-27 15:13:21 +0200

    Aktualisiere test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-comment.comment("write comment here")
+comment.writeComment("write comment here")
 
-comment.comment("A visible user comment")
+comment.writeComment("A visible user comment")
